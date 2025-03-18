eb85f4dfedb9d6158d8a973818f340b520da8b78
Sunshine <yewhang2004@gmail.com>
2025-03-18 19:08:05 +0800

    a cube

--- core/src/main/java/sep/fastAndFuriousStephenHawkingDrift/Main.java
+++ core/src/main/java/sep/fastAndFuriousStephenHawkingDrift/Main.java
@@ -2,7 +2,6 @@ package sep.fastAndFuriousStephenHawkingDrift;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-<<<<<<< HEAD
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
@@ -59,35 +58,5 @@ public class Main extends ApplicationAdapter {
     @Override
     public void dispose(){
         model.dispose();
-=======
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
-
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
-
-    @Override
-    public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-    }
-
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
->>>>>>> 356e8b1 (initial commit, THE BEGINNING OF GREATNESSgit add .)
     }
 }
